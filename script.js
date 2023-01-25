85eda8c494bb3fd5b32f47a514b0e59db868f21c
urgt <urinbaevg@gmail.com>
2023-01-25 20:19:42 +0500

    video-fix

--- script.js
+++ script.js
@@ -1,9 +1,7 @@
 
-
-
-function imgToVideo() {
+function imgToVideo(element) {
     console.log(1)
-    document.getElementsByClassName("video_wrap").innerHTML = `<p>hi</p>`;
+    element.innerHTML = `<iframe width="100%" height="500px" src="https://www.youtube.com/embed/vG7wYkkRUDs" title="FLYING OVER INDONESIA (4K Video UHD) - Peaceful Music With Beautiful Nature Video For Stress Relief" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>`;
 
 }
 
